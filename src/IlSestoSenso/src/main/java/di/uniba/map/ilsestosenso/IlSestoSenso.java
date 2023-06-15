bcddf1697b764518bc1128c574c3baa4aab7fa00
Raffaele Papeo <raffaelepapeo@MacBook-Air-di-Raffaele.local>
2023-06-15 10:42:20 +0200

    commit iniziale

---
+++ src/IlSestoSenso/src/main/java/di/uniba/map/ilsestosenso/IlSestoSenso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.uniba.map.ilsestosenso;
+
+/**
+ *
+ * @author raffaelepapeo
+ */
+public class IlSestoSenso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
